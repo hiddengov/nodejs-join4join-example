59c0e486c1d50af0a68d443ca7e158b2ec879570
hiddengov <alvin08vt@gmail.com>
2026-02-08 20:48:49 -0500

    Update bot token, invite link, and admin/owner IDs

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "YOUR BOT TOKEN",
-    bot_invite_link: "YOUR BOT INVITE LINK",
+    token: "MTQ2NTU3NzI4Nzc0MjEzMjI2Ng.GlPmPa.du1cd9AXqlg3Nj-7POgq29hRQMzVia1n5jtnbw",
+    bot_invite_link: "https://discord.com/oauth2/authorize?client_id=1465577287742132266&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fdiscord.gg%2FSyXkcxJEmF&integration_type=0&scope=guilds+guilds.join+bot+connections+applications.commands+messages.read",
     url: "https://join4join.xyz",
     footer: "https://join4join.xyz • (C) 2024 Join4Join",
     color: "#e84e4b",
-    admin: [""],
-    owner: [""],
-    api_key: "YOUR API KEY (https://join4join.xyz/dashboard/settings?developer)",
+    admin: ["1463336188583743518"],
+    owner: ["1463336188583743518"],
+    api_key: "YOUR API KEY (0cb354d993c34f2caf690015c50d8033)",
     api_url: "https://join4join.xyz",
 }
